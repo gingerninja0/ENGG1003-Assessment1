13f16dc6c3d485862d118a55322349dc0fcc5b16
Andrew Marshall <andrew.d.marshall00@gmail.com>
2019-04-09 04:01:34 +0000

    added file in
    to do file out and string key/int key decider

--- Assessment1.c
+++ Assessment1.c
@@ -8,9 +8,7 @@ file i/o
 sustitution cypher encode and decode
 add dictionary and use it in caesar cypher decryption without key so only the correct output is printed, so there is not 25*/
 
-void mode_auto(int select, char *x,char *z, int k, int key, char *subkeyen, char *subkeyde, int intmode);
-
-void mode_select(int select, char *x, char *z, int k, int key, char *subkeyen, char *subkeyde, int intmode);
+void mode_auto(int select, char *x,char *z, int k, int key, char *subkeyen, char *subkeyde);
 
 void copy_array_yx(char *x, char *y, int ky);
 
@@ -26,48 +24,64 @@ void decode_caesarwkey(char *x, int k, int key);
 
 void decode_caesarwokey(char *x, char *z, int k);
 
-void encode_substitution(char *x, int k, char *subrefi, int intitialselect, int intmode);
+void encode_substitution(char *x, int k, char *subrefi);
 
-void create_substitution_key(char *subkeyen, char *subkeyde, int intmode);
+void create_substitution_key(char *subkeyen, char *subkeyde);
 
-void decode_substitution(char *x, int k, char *subkeyen, char *subkeyde, int intmode);
+void decode_substitution(char *x, int k, char *subkeyen, char *subkeyde);
 
 int main()
-{   
+{
+    char x[1000];
+    int k=sizeof(x)/sizeof(char); //finds amount of elements in char which is used as a constant
+    
+    
+    /*
+     * the file layout is:
+     * 
+     * mode
+     * key
+     * message to encode/decode
+     */
+    FILE *input;
+    
+    char c;
+    int key,select;
+    input=fopen("input.txt", "r");
+    
+    if(input==NULL){
+        perror("fopen()");
+        return 0;
+    }
+    
+    fscanf(input, "%d", &select);
+    
+    fscanf(input, "%d", &key); //make it chose if 1-3 int key, if not then string key of random alphabet
+    
+    for(int i=0; i<k; i++){
+        fscanf(input, "%c", &c);
+        if(feof(input)!=0) break;
+        if(islower(c)){
+            c-=32;
+        }
+        x[i]=c;
+    }
+    fclose(input);
+    
+    printf("Message from file\n\n"); //prints the message after being converted to capitals
+    print_code(x,k);
+    
+
     //intialised array, used to quickly input a large message
-    char y[]="PDA MQFBH ENKUY RKC GQJLO KTAN PDA IZXW SKV";
-    int ky=sizeof(y)/sizeof(char); //amount of elements in intialised array
+    //char y[]="PDA MQFBH ENKUY RKC GQJLO KTAN PDA IZXW SKV";
+    //int ky=sizeof(y)/sizeof(char); //amount of elements in intialised array
     
     //initialised selection and key, used instead of an interactive menu, as it is quicker and easier
-    int select=5; //list selections here!
-    
-    int intmode;
-    
-    int key=9; //key used for decoding and encoding quickly
     //               abcdefghijklmnopqrstuvwxyz   the alphabet
     char subkeyen[]="ZEBRASCDFGHIJYKLMNOPQTUVWX"; //reference list for substitution encoder/decode
     
-    if(select==0){
-        if(sizeof(subkeyen)>1){
-            intmode=2;
-        }
-        else {
-            intmode=1;
-        }
-    }
-    //will include an if for when there is a file being included
-    else if(sizeof(subkeyen)>1) {
-        intmode=2;
-    }
     
     
-    
-    
-    char x[1000];
-    int k=sizeof(x)/sizeof(char); //finds amount of elements in char which is used as a constant
-    if(ky>1){
-        k=ky;
-    }
     //array used to compare to x in brute force decoding of caesar cypher
     char z[k];
     
@@ -75,31 +89,18 @@ int main()
     
     char subkeyde[26];
     
-    /*if an array is initialised, then it skips using scan_code, the copies string to array x using copy_array_yx
-    so it can be changed in the program*/
-    if(ky!=1){
-        copy_array_yx(x,y,k);
-        print_code(x,k);
-    } 
-    /*else read user message using scan_code()*/
-    else    {
-        scan_code(x,k);
-    }
     
-    /*if there is not mode selected through initialisation then read user inputs using mode_select()*/
-    if(select==0){
-        mode_select(select,x,z,k,key,subkeyen,subkeyde,intmode);
-    }
-    /*else there must be an initialised selection, use mode_auto() to show relevant mode selected*/
-    else {
-        mode_auto(select,x,z,k,key,subkeyen,subkeyde,intmode);
-    }
+    mode_auto(select,x,z,k,key,subkeyen,subkeyde);
+    
+
+
+
     
     return 0;
 }
 
-void decode_substitution(char *x, int k, char *subkeyen, char *subkeyde, int intmode){
-    create_substitution_key(subkeyen,subkeyde,intmode);
+void decode_substitution(char *x, int k, char *subkeyen, char *subkeyde){
+    create_substitution_key(subkeyen,subkeyde);
     int code;
     for(int n=0; n<k; n++){
         if(isupper(x[n])){
@@ -111,39 +112,19 @@ void decode_substitution(char *x, int k, char *subkeyen, char *subkeyde, int int
 
 
 
-void encode_substitution(char *x, int k, char *subkeyen, int select, int intmode){
+void encode_substitution(char *x, int k, char *subkeyen){
     int code;
-    char subkeysc[26]; //what scanf will input too
-    
-    if(intmode==1){
-        printf("Input alphabet key: ");
-        scanf("%s",&subkeysc);
-    }
-    else if(intmode==2){
-        printf("Using initialised key: %s\n\n",subkeyen);
-    }
-    //this is where it will say it is using key from file
-    
     for(int n=0; n<k; n++){
         if(isupper(x[n])){
             code=x[n]-65;
-        switch(intmode){
-            case 1: //encode from scanf
-                x[n]=subkeysc[code];
-                break;
-            case 2: //encode from initialised string
-                x[n]=subkeyen[code];
-                break;
-            case 3: //encode from file
-                break;
-            }
+            x[n]=subkeyen[code];
         }    
     }
 }
 
 /*uses the initialised value for select to select mode and call relevant function, includes relevant prints so it makes
 sense in the context of auto select*/
-void mode_auto(int select, char *x,char *z, int k, int key, char *subkeyen, char *subkeyde, int intmode){
+void mode_auto(int select, char *x,char *z, int k, int key, char *subkeyen, char *subkeyde){
     switch(select){
         case 1:
             printf("Encoding message using caesar cypher using key: %d\n\n",key);
@@ -161,48 +142,14 @@ void mode_auto(int select, char *x,char *z, int k, int key, char *subkeyen, char
             break;
         case 4:
             printf("Encoding message using substitution cypher\n\n");
-            encode_substitution(x,k,subkeyen,select,intmode);
+            encode_substitution(x,k,subkeyen);
             print_code(x,k);
             break;
         case 5:
             printf("Decoding message using substitution cypher using substitution key");
-            decode_substitution(x,k,subkeyen,subkeyde,intmode);
+            decode_substitution(x,k,subkeyen,subkeyde);
             print_code(x,k);
             break;
-    }
-}
-
-/*prompts user to select mode and calls relevant function*/
-void mode_select(int select, char *x, char *z, int k, int key, char *subkeyen, char *subkeyde, int intmode){
-    printf("Mode Selection Menu\n");
-    printf("1: encode using caesar cypher\n");
-    printf("2: decode caesar cypher with known key\n");
-    printf("3: decode caesar cypher using brute force\n");
-    printf("4: encode using substitution cypher\n");
-    printf("5: decode using substitution cypher with known substitution key\n\n");
-    printf("Select mode: ");
-    scanf("%d",&select);
-    printf("\n");
-    switch(select){
-        case 1:
-            encode_caesar(x,k,key); //call encode_caesar() to encode message
-            print_code(x,k);  //prints encoded message using print_code()
-            break;
-        case 2:
-            decode_caesarwkey(x,k,key); //call caesar decode_caesarwkey() function
-            print_code(x,k); //prints decoded message using print_code()
-            break;
-        case 3:
-            decode_caesarwokey(x,z,k); //call decode_caesarwokey() to decode message with out a key being provided
-            break;
-        case 4:
-            encode_substitution(x,k,subkeyen,select,intmode);
-            print_code(x,k);
-            break;
-        case 5:
-            decode_substitution(x,k,subkeyen,subkeyde,intmode);
-            print_code(x,k);
-            break;    
     }
 }
 
@@ -229,7 +176,7 @@ void copy_array_yx(char *x, char *y, int k){
     printf("Using initialised message\n\n");
 }
 
-void create_substitution_key(char *subkeyen, char *subkeyde, int intmode){
+void create_substitution_key(char *subkeyen, char *subkeyde){
     int l;
     for(int n=0; n<26; n++){
         if(isupper(subkeyen[n])){
@@ -261,7 +208,6 @@ void scan_code(char *x, int k){
 
 /*prints array to element k or if there is an initialised array then ky is used to make it more efficient*/
 void print_code(char *x, int k){ //update this to remove int k and fix comments !!!!!!!!!!!!!!!!!!!!!!
-    
     for(int i=0; x[i]!=0; i++){
         printf("%c",x[i]); //prints array element x[i] until i=k-1, then stop as this is the end of the array
     }
@@ -271,27 +217,18 @@ void print_code(char *x, int k){ //update this to remove int k and fix comments 
 /*encodes array x by a key which is inputed by the user or initialised, finishes at final element k, unless using 
 initialised message, then ky is used to prevent overflow*/
 void encode_caesar(char *x, int k, int key){
-    //input key if one is not initialised at start
-    if(key==0){
-        printf("Select key to use to encode: ");
-        scanf("%d",&key);
-    }
     for(int n=0; n<k; n++){
-        //this only encodes capital letters, leaves punctuation and spaces unendoded
+        //this only encodes capital letters, leaves punctuation and spaces unencoded
         if(isupper(x[n])){
             x[n]=(((x[n]-65)+key)%26)+65; //encode formula, replaces letter x[n] with output from encryption formula
         }
     }
+
 }
 
 /*decodes array x by a key which is inputed by the user or initialised, finishes at final element k, unless using 
 initialised message, then ky is used to prevent overflow*/
 void decode_caesarwkey(char *x, int k, int key){
-    //input key if one is not initialised at start
-    if(key==0){
-        printf("Select key to use to decode: ");
-        scanf("%d",&key);
-    }
     for(int n=0; n<k; n++){
         //this only decodes capital letters, leaves punctuation and spaces undecoded
         if(isupper(x[n])){
